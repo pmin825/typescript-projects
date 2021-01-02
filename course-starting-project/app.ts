41e6f03043b72aef0c79d33259af86475f10112a
pmin825 <pmin.0825@gmail.com>
2021-01-02 15:59:07 -0800

    function types

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -10,4 +10,12 @@ function printResult(num: number): void {
 
 console.log(printResult(add(5, 12)));
 
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+
+
+console.log(combineValues(8 , 8));
+
 // let someValue: undefined;
