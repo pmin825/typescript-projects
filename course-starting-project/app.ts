0cadb1ecb6f89fec9705daf17abb992aab90cac9
pmin825 <pmin.0825@gmail.com>
2021-01-02 15:39:59 -0800

    type aliases and custom types

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,15 +1,30 @@
-function combine(input1: number | string, input2: number | string) {
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text'
+
+function combine(
+    input1: Combinable, 
+    input2: Combinable, 
+    resultConversion: ConversionDescriptor
+) {
     let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }   else {
         result = input1.toString() + input2.toString();
     }
     return result;
+    // if (resultConversion === 'as-number') {
+    //     return +result; 
+    // }   else {
+    //     return result.toString();
+    // }
 }
 
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
 
-const combinedNames = combine('max', 'anna');
+const combinedStringAges = combine('30', '26', 'as-number')
+console.log(combinedStringAges)
+
+const combinedNames = combine('max', 'anna', 'as-text');
 console.log(combinedNames);
