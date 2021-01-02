cab42c67c81549a8f360c8317c1def4f0588a10d
pmin825 <pmin.0825@gmail.com>
2021-01-02 15:24:57 -0800

    union types

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,30 +1,15 @@
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-
-enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 200 };
-
-const person = {
-    name: 'max',
-    age: 30,
-    hobbies: ['sports', 'cooking'],
-    role: Role.ADMIN
-};
-
-// person.role.push('admin');
-// person.role[1] = 10;
-
-// person.role = [0, 'admin', 'user'];
-
-let favoriteActivities: string[];
-favoriteActivities = ['sports']
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase());
+function combine(input1: number | string, input2: number | string) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }   else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
 
-if (person.role === Role.AUTHOR) {
-    console.log('is author')
-}
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
+
+const combinedNames = combine('max', 'anna');
+console.log(combinedNames);
