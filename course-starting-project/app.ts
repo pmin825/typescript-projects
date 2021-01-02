05e291b6c17154c5b9e08763b1a8013f5c5b6aa8
pmin825 <pmin.0825@gmail.com>
2021-01-02 15:17:25 -0800

    work on arrays objects tuples enums

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,15 +1,30 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    const result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }   else {
-        return result;
-    }
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 200 };
+
+const person = {
+    name: 'max',
+    age: 30,
+    hobbies: ['sports', 'cooking'],
+    role: Role.ADMIN
+};
+
+// person.role.push('admin');
+// person.role[1] = 10;
+
+// person.role = [0, 'admin', 'user'];
+
+let favoriteActivities: string[];
+favoriteActivities = ['sports']
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
 }
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-
-add(number1, number2, printResult, resultPhrase);
+if (person.role === Role.AUTHOR) {
+    console.log('is author')
+}
