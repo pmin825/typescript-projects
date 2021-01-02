d59de779b9515a05297d8f94bdbf751ae2fb1382
pmin825 <pmin.0825@gmail.com>
2021-01-02 15:53:18 -0800

    understanding void and undefined

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,30 +1,13 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text'
-
-function combine(
-    input1: Combinable, 
-    input2: Combinable, 
-    resultConversion: ConversionDescriptor
-) {
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }   else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    // if (resultConversion === 'as-number') {
-    //     return +result; 
-    // }   else {
-    //     return result.toString();
-    // }
+function add(n1: number, n2: number) {
+    return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
+function printResult(num: number): void {
+    //void just means there is no return statement
+    //undefined when there is return but no value
+    console.log('Result: ' + num);
+}
 
-const combinedStringAges = combine('30', '26', 'as-number')
-console.log(combinedStringAges)
+console.log(printResult(add(5, 12)));
 
-const combinedNames = combine('max', 'anna', 'as-text');
-console.log(combinedNames);
+// let someValue: undefined;
