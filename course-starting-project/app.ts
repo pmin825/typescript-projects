174b413d02a279451e25e6dc48947ee4223ad099
pmin825 <pmin.0825@gmail.com>
2021-01-02 16:10:51 -0800

    function types and callbacks

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -5,7 +5,12 @@ function add(n1: number, n2: number) {
 function printResult(num: number): void {
     //void just means there is no return statement
     //undefined when there is return but no value
-    console.log('Result: ' + num);
+    console.log("Result: " + num);
+}
+
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result);
 }
 
 console.log(printResult(add(5, 12)));
@@ -15,7 +20,11 @@ let combineValues: (a: number, b: number) => number;
 combineValues = add;
 // combineValues = printResult;
 
-
-console.log(combineValues(8 , 8));
+console.log(combineValues(8, 8));
 
 // let someValue: undefined;
+
+addAndHandle(10, 20, (result) => {
+    console.log(result);
+    return result;
+});
