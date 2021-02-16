dbbe90046500e65edf56ec2805e613183bcc5a90
pmin825 <pmin.0825@gmail.com>
2021-02-15 19:27:26 -0800

    add additional routes for crud operations

--- node-express-ts/src/app.ts
+++ node-express-ts/src/app.ts
@@ -5,6 +5,8 @@ import todoRoutes from "./routes/todos";
 
 const app = express();
 
+app.use(json());
+
 app.use("/todos", todoRoutes);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
--- node-express-ts/src/routes/todos.ts
+++ node-express-ts/src/routes/todos.ts
@@ -1,15 +1,20 @@
 import { Router } from "express";
 
-import { createTodo } from "../controllers/todo"
+import {
+    createTodo,
+    getTodos,
+    updateTodo,
+    deleteTodo,
+} from "../controllers/todo";
 
 const router = Router();
 
 router.post("/", createTodo);
 
-router.get("/");
+router.get("/", getTodos);
 
-router.patch("/:id");
+router.patch("/:id", updateTodo);
 
-router.delete("/:id");
+router.delete("/:id", deleteTodo);
 
 export default router;
