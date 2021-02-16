9fbfee0eecd9f7b42304d8f9b81bbd87694822ab
pmin825 <pmin.0825@gmail.com>
2021-02-15 17:53:42 -0800

    setup project, add middleware and types, create controller and model

---
+++ node-express-ts/dist/routes/todos.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const todo_1 = require("../controllers/todo");
+const router = express_1.Router();
+router.post("/", todo_1.createTodo);
+router.get("/");
+router.patch("/:id");
+router.delete("/:id");
+exports.default = router;
